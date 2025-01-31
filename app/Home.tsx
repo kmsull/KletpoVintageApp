c2182f35e5420e1153ab25cd4dd4d240b43e209d
Kenneth <kmsullivanjr@uri.edu>
2025-01-31 00:49:58 -0500

    Created starting structure for Shopping interface

---
+++ app/Home.tsx
@@ -0,0 +1,95 @@
+import { StyleSheet, ScrollView } from "react-native";
+import { Text, View } from "@/components/Themed";
+
+export default function TabOneScreen() {
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>Shirts</Text>
+      <ScrollView
+        style={styles.shopSection}
+        horizontal={true}
+        contentOffset={{ x: -2, y: -8 }}
+        showsHorizontalScrollIndicator={false}
+      >
+        <View style={styles.shopCard}>
+          <Text>This one</Text>
+        </View>
+        <View style={styles.shopCard}>
+          <Text>This one</Text>
+        </View>
+        <View style={styles.shopCard}>
+          <Text>This one</Text>
+        </View>
+        <View style={styles.shopCard}>
+          <Text>This one</Text>
+        </View>
+      </ScrollView>
+      <Text style={styles.title}>Pants</Text>
+      <ScrollView
+        style={styles.shopSection}
+        horizontal={true}
+        contentOffset={{ x: -2, y: -8 }}
+        showsHorizontalScrollIndicator={false}
+      >
+        <View style={styles.shopCard}>
+          <Text>This one</Text>
+        </View>
+        <View style={styles.shopCard}>
+          <Text>This one</Text>
+        </View>
+        <View style={styles.shopCard}>
+          <Text>This one</Text>
+        </View>
+        <View style={styles.shopCard}>
+          <Text>This one</Text>
+        </View>
+      </ScrollView>
+      <Text style={styles.title}>Jackets</Text>
+      <ScrollView
+        style={styles.shopSection}
+        horizontal={true}
+        contentOffset={{ x: -2, y: -8 }}
+        showsHorizontalScrollIndicator={false}
+      >
+        <View style={styles.shopCard}>
+          <Text>This one</Text>
+        </View>
+        <View style={styles.shopCard}>
+          <Text>This one</Text>
+        </View>
+        <View style={styles.shopCard}>
+          <Text>This one</Text>
+        </View>
+        <View style={styles.shopCard}>
+          <Text>This one</Text>
+        </View>
+      </ScrollView>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "95%",
+    marginVertical: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  shopSection: {
+    width: "100%",
+    maxHeight: 300,
+    paddingVertical: 32,
+  },
+  shopCard: {
+    width: 200,
+    height: 250,
+    shadowOpacity: 2,
+    shadowOffset: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 4,
+    marginHorizontal: 10,
+  },
+});
