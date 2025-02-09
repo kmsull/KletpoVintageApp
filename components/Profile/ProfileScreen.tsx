e2b1cf4e4d5ecd4a83bf85267929b7e05f671800
Kenneth <kmsullivanjr@uri.edu>
2025-02-09 02:53:19 -0500

    Started Profile Page styling

--- components/Profile/ProfileScreen.tsx
+++ components/Profile/ProfileScreen.tsx
@@ -1,29 +1,37 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Image } from "react-native";
 
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 
 export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Profile Screen</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.userImage}>
+                <Image source={{ uri: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png" }} style={{ borderRadius: 60, height: 120, width: 120 }} />
+            </View>
+
+            <View style={styles.nameContainer}>
+                <Text style={styles.name}>John Smith</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
+    container: {
+        alignItems: "center",
+    },
+    nameContainer: {
+        width: "100%",
+        alignItems: "center",
+    },
+    name: {
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+    userImage: {
+        width: "100%",
+        alignItems: "center",
+        paddingVertical: "5%",
+    },
 });
