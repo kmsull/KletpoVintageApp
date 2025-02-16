796d940d33c745c7f36775f68159a4584aa69b9d
Kenneth <kmsullivanjr@uri.edu>
2025-02-16 02:16:39 -0500

    began styling shop card to resemble a fine arts display

--- components/ShopCard/ShopCard.tsx
+++ components/ShopCard/ShopCard.tsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { StyleSheet, Image, TouchableOpacity } from "react-native";
+import React, { useContext, useState } from "react";
+import { StyleSheet, Image, TouchableOpacity, Modal } from "react-native";
 import { Text, View, Card, ClearView } from "@/components/Themed";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { CartContext } from "@/contexts/CartContext";
@@ -20,6 +20,7 @@ interface Product {
 }
 
 export default function ShopCard({ product }: ShopCardProps) {
+    const [showDescription, setOpenModal] = useState(false);
     const { id, name, img, description, size, price } = product;
 
     const { addItem } = useContext(CartContext);
@@ -30,30 +31,54 @@ export default function ShopCard({ product }: ShopCardProps) {
         addItem({ id, name, img, description, size, price });
     };
 
+    const openDescriptionModal = () => {
+        setOpenModal(true);
+    };
+
+    const closeDiscriptionModal = () => {
+        setOpenModal(false);
+    };
+
     return (
-        <Card style={{ borderRadius: 32, marginHorizontal: 10, paddingHorizontal: 10 }}>
-            <ClearView style={{ paddingVertical: 6 }}>
-                <Text style={{ fontSize: 24, textAlign: "center" }}>{name}</Text>
-            </ClearView>
+        <Card style={{ borderRadius: 32, marginHorizontal: 18, paddingHorizontal: 10, backgroundColor: "transparent", width: 300 }}>
             <ClearView style={{ paddingHorizontal: 10 }}>
                 <Image source={frame} style={{ height: 260, width: 260 }} />
                 <FontAwesome5 name="tshirt" size={96} style={{ position: "absolute", top: "30%", left: "30%" }} />
             </ClearView>
-            <ClearView style={{ paddingVertical: 4, maxWidth: 260, }}>
-                <Text style={{ fontSize: 16, fontWeight: "600" }}>{description}</Text>
-            </ClearView>
-            <ClearView style={styles.buttonContainer}>
-                <View style={[styles.cartButton, { backgroundColor: button, borderColor: button, paddingHorizontal: 24 }]}>
-                    <Text style={{ color: "white", fontWeight: "bold" }}>{size}</Text>
-                </View>
-                <View style={[styles.cartButton, { backgroundColor: button, borderColor: button, paddingHorizontal: 12 }]}>
-                    <Text style={{ color: "white", fontWeight: "bold" }}>${price}</Text>
-                </View>
-                <TouchableOpacity style={[styles.addToCartButton, { backgroundColor: button, borderColor: button }]} onPress={onAddItem}>
-                    <FontAwesome5 name="shopping-cart" size={24} style={{ color: "white" }} />
-                    <Text style={{ color: "white", fontWeight: "bold" }}> + </Text>
+            <ClearView style={styles.infoContainer}>
+                <TouchableOpacity style={styles.desCard} onPress={openDescriptionModal}>
+                    <ClearView style={{ paddingVertical: 6 }}>
+                        <Text style={{ fontSize: 16, fontStyle: "italic" }}>
+                            {name} ({size})
+                        </Text>
+                    </ClearView>
+                    <ClearView style={{ paddingVertical: 4, maxWidth: 260 }}>
+                        <Text style={{ fontSize: 12, color: "gray", fontStyle: "italic", fontWeight: "600" }}>{description}</Text>
+                    </ClearView>
                 </TouchableOpacity>
+                <ClearView style={styles.buttonContainer}>
+                    <View style={[styles.addToCartButton, { backgroundColor: button, borderColor: button, paddingHorizontal: 12 }]}>
+                        <Text style={{ color: "white", fontWeight: "bold" }}>${price}</Text>
+                    </View>
+                    <TouchableOpacity style={[styles.addToCartButton, { backgroundColor: button, borderColor: button }]} onPress={onAddItem}>
+                        <FontAwesome5 name="shopping-cart" size={24} style={{ color: "white" }} />
+                        <Text style={{ color: "white", fontWeight: "bold" }}> + </Text>
+                    </TouchableOpacity>
+                </ClearView>
             </ClearView>
+            <Modal style={styles.descriptionModal} visible={showDescription} onClose={closeDiscriptionModal}>
+                <ClearView style={styles.modalView}>
+                    <ClearView style={styles.modalInfo}>
+                        <Text style={{ fontSize: 24, fontWeight: "bold" }}>{name}</Text>
+                        <Text style={{ fontSize: 16, fontStyle: "italic" }}>{description}</Text>
+                        <Text style={{ fontSize: 14, color: "gray" }}>Size: {size}</Text>
+                        <Text style={{ fontSize: 14, color: "gray" }}>Price: ${price}</Text>
+                        <TouchableOpacity onPress={closeDiscriptionModal} style={styles.closeButton}>
+                            <Text style={styles.closeButtonText}>Close</Text>
+                        </TouchableOpacity>
+                    </ClearView>
+                </ClearView>
+            </Modal>
         </Card>
     );
 }
@@ -62,16 +87,24 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
-    buttonContainer: {
+    infoContainer: {
         flexDirection: "row",
+        width: "100%",
         justifyContent: "space-between",
-        alignItems: "center",
-        paddingHorizontal: 10,
-        paddingVertical: 12,
+        gap: 8,
     },
-    cartButton: {
+    desCard: {
         borderRadius: 16,
-        paddingVertical: 9,
+        borderWidth: 2,
+        borderColor: "white",
+        width: "70%",
+        backgroundColor: "white",
+        padding: 8,
+    },
+    buttonContainer: {
+        flexDirection: "column",
+        justifyContent: "center",
+        gap: 8,
     },
     addToCartButton: {
         borderRadius: 16,
